99225fe36ba5f1b4cf3b01b96a530b8408a0d80e
Roger Michaud <cptjohn47@gmail.com>
2013-08-28 12:30:16 -0500

    Merge cm10.2

--- aocp.mk
+++ aocp.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common AoCP stuff.
 $(call inherit-product, vendor/aocp/config/gsm.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/aocp/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common AoCP stuff.
 $(call inherit-product, vendor/aocp/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/IMM76D/UCLI3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 IMM76D UCLI3 release-keys"
